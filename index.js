e3bf07d64910ab8c8094b6c0eb8bb73f9bcf6689
Oleg Prohazko <prohazko@rightech.io>
2019-09-20 01:50:45 +0300

    main-catch

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 
-
 async function main() {
   const zigbee = await require('./src/zigbee').init();
   const core = await require('./src/core').connect();
 
   if (process.argv.includes('--repl')) {
     const repl = require('./src/repl');
-    repl.start({
-      zigbee,
-      core
-    });
+    repl.start({ zigbee, core });
   }
 }
 
-main();
+main().catch(err => {
+  console.log('main err:', err);
+  process.exit(1);
+});
